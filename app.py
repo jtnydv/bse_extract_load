081383940155ca6de58bfad1fb9c490f10e3db97
Jatin Yadav <jatin.yadav1307@gmail.com>
2018-03-20 23:05:12 +0530

    Heroku: Minor changes

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ class BSEData(object):
             response.append(data)
         return response
 
-cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
-cherrypy.quickstart(BSEData())
+
+config = {
+    'global': {
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': int(os.environ.get('PORT', 5000)),
+    }
+}
+
+cherrypy.quickstart(BSEData(), '/', config=config)
